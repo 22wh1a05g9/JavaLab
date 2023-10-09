603e0e36a100d8eaeae95f624ddc492bfb5023b2
Sravya_22wh1a05g9 <130379309+22wh1a05g9@users.noreply.github.com>
2023-10-09 20:20:40 +0530

    Create constructor1.java

---
+++ constructor1.java
@@ -0,0 +1,13 @@
+//constructor with parameters
+public class constructorwp{
+int x;
+public constructorwp(int y)
+{
+x=10;
+y=x;
+}
+public static void main(String args[]){
+constructorwp object=new constructorwp();
+System.out.println(object.5);
+}
+}
