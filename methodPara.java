465c255c1e7ee831e776f3c0156ac2021ffb4781
Sravya_22wh1a05g9 <130379309+22wh1a05g9@users.noreply.github.com>
2023-10-09 20:19:45 +0530

    Create methodPara.java

---
+++ methodPara.java
@@ -0,0 +1,9 @@
+public class ImpMethod{
+    public int addition(int x, int y){
+        return (x+y);
+    }
+    public static void main(String args []){
+        ImpMethod a=new ImpMethod();
+        System.out.println(a.addition(2,3));
+    }
+}
