7dc6db09ecb16162da971cfceb984afbf5812a05
Sravya_22wh1a05g9 <130379309+22wh1a05g9@users.noreply.github.com>
2023-10-09 20:22:44 +0530

    Create constructor2.java

---
+++ constructor2.java
@@ -0,0 +1,10 @@
+public class constructorwp {
+    int x;
+    public constructorwp() {
+        x = 10;
+    }
+    public static void main(String args[]) {
+        constructorwp object = new constructorwp();
+        System.out.println(object.x); 
+    }
+}
